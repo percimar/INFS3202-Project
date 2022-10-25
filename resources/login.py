a214f72864274330bf23f9535a90533e6ae56ded
Asmar Siddiqui <Asmar@email.com>
2022-10-26 02:35:59 +0300

    fix login validation

--- resources/login.py
+++ resources/login.py
@@ -12,6 +12,13 @@ class LoginResource(Resource):
         if "user" not in request.form:
             return {"message": "Please provide a user"}, HTTPStatus.BAD_REQUEST
         user_name = request.form["user"]
+
+        if len(user_name) == 0:
+            return {"message": "Please provide a user"}, HTTPStatus.BAD_REQUEST
+
+        if len(user_name) < 4:
+            return {"message": "user name too short"}, HTTPStatus.BAD_REQUEST
+
         user = User.get_or_create_user(user_name)
         # set a cryptographically secured cookie
         session["user"] = user.id
